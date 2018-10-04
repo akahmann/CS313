4e617f0bd3af46a41207b99c922964f3cc0c3380
akahmann <akahmann93@gmail.com>
2018-10-04 16:21:08 -0600

    Cookies!

--- web/cookies.php
+++ web/cookies.php
@@ -2,6 +2,16 @@
 
 //setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
 session_start();
+
+if(isset($_SESSION["counter"])) {
+   $_SESSION["counter"]++;
+}
+else {
+   $_SESSION["counter"] = 1;
+}
+
+$visits = $_SESSION["counter"];
+
 ?>
 
 <!DOCTYPE html>
@@ -15,11 +25,13 @@ Where's the cookie???
 
 <?php
 
-$_SESSION["counter"] = 0;
+//$_SESSION["counter"] = 0;
 
-if(isset($_SESSION["counter"])) {
-   echo "\nIt is set";
-}
+//$user = $_SESSION["user"];
+
+//$user = count()
+
+echo "You have been here $visits times";
 
 ?>
 
