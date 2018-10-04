0b1a18991f3cf5c2f108dc93eba079ba58409f1d
akahmann <akahmann93@gmail.com>
2018-10-04 15:59:10 -0600

    Added cookie and squirreltopia

---
+++ web/browseSquirrels.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+
+<h1>Squirreltopia</h1>
+
+<img src="normalSquirrel.jpg" alt="Normal Squirrel"> <br>
+<img src="happySquirrel.jpg" alt="Happy Squirrel"> <br>
+<img src="heroicSquirrel.jpg" alt="Heroic Squirrel"> <br>
+<img src="evilSquirrel.jpg" alt="Evil Squirrel"> <br>
+<img src="fatSquirrel.jpg" alt="Fat Squirrel"> <br>
+<img src="knightSquirrel.jpg" alt="Knight Squirrel"> <br>
+<img src="supermanSquirrel.jpg" alt="Superman Squirrel"> <br>
+<img src="thorSquirrel.jpg" alt="Thor Squirrel"> <br>
+<img src="giantSquirrel.jpg" alt="Giant Squirrel"> <br>
+<img src="hyenaSquirrel.jpg" alt="Hyena Squirrel"> <br>
+<img src="buffSquirrel.jpg" alt="Buff Squirrel"> <br>
+<img src="invisibleSquirrel.jpg" alt="Invisible Squirrel"> <br>
+
+</body>
+</html>
---
+++ web/cookies.php
@@ -0,0 +1,17 @@
+<?php
+
+setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+
+Where's the cookie???
+
+</body>
+</html>
