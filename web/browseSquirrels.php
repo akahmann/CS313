392ee6f95b90b5593e24ee1ca628ecc313c4e210
akahmann <akahmann93@gmail.com>
2018-10-08 14:52:52 -0600

    Design

--- web/browseSquirrels.php
+++ web/browseSquirrels.php
@@ -6,8 +6,8 @@ session_start();
 //?key=ProductName&value=ValueAmount
 //%20 = space
 
-require 'addSquirrels.php';
-echo $item;
+// require 'addSquirrels.php';
+// echo $item;
 
 $squirrels = array();
 if(!isset($_SESSION["squirrels"]))
