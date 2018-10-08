ff99d449802a4832994b939b6810afb3c078fd78
akahmann <akahmann93@gmail.com>
2018-10-08 12:06:46 -0600

    Design

--- web/browseSquirrels.php
+++ web/browseSquirrels.php
@@ -56,6 +56,12 @@ if(!isset($_SESSION["squirrels"]))
 </div>
 <br>
 <div class="flex">
+   <p><b>Normal Squirrel:</b> If you feel like being boring purchase this normal squirrel!</p>
+   <p><b>Happy Squirrel:</b> You will never be sad when this squirrel is constantly by your side!</p>
+   <p><b>Heroic Squirrel:</b> This squirrel doesn't fear anything, not even a 30 ton pitbull.</p>
+</div>
+<br>
+<div class="flex">
    <form action="addSquirrels.php" method="post">
       <input type="submit" class="btn" value="Add Normal Squirrel">
       <input type="hidden" name="item" value="000">
@@ -75,6 +81,12 @@ if(!isset($_SESSION["squirrels"]))
    <img src="evilSquirrel.jpg" alt="Evil Squirrel" class="triplePic">
    <img src="fatSquirrel.jpg" alt="Fat Squirrel" class="triplePic">
    <img src="knightSquirrel.jpg" alt="Knight Squirrel" class="triplePic">
+</div>
+<br>
+<div class="flex">
+   <p><b>Evil Squirrel:</b> Purchase this squirrel with caution, it has been known to cause the end of the world.</p>
+   <p><b>Fat Squirrel:</b> Once you get this squirrel you will never have to ask who ate your cookies again!</p>
+   <p><b>Knight Squirrel:</b> You've heard of Knights of the Round table, but have you heard of Squirrels of the Round Table?</p>
 </div>
 <br>
 <div class="flex">
@@ -100,6 +112,12 @@ if(!isset($_SESSION["squirrels"]))
 </div>
 <br>
 <div class="flex">
+   <p><b>Superman Squirrel:</b> The is the most over powered and boring squirrel we have to offer!</p>
+   <p><b>Thor Squirrel:</b> BRING ME THANOS!!!!!!</p>
+   <p><b>Giant Squirrel:</b> If you ever wanted to reenact Godzilla, but with a Squirrel instead, we got the Squirrel for you!</p>
+</div>
+<br>
+<div class="flex">
    <form action="addSquirrels.php" method="post">
       <input type="submit" class="btn" value="Add Superman Squirrel">
       <input type="hidden" name="item" value="006">
@@ -119,6 +137,12 @@ if(!isset($_SESSION["squirrels"]))
    <img src="hyenaSquirrel.jpg" alt="Hyena Squirrel" class="triplePic">
    <img src="buffSquirrel.jpg" alt="Buff Squirrel" class="triplePic">
    <img src="invisibleSquirrel.jpg" alt="Invisible Squirrel" class="triplePic">
+</div>
+<br>
+<div class="flex">
+   <p><b>Hyena Squirrel:</b> This squirrel has a really great sense of humor.</p>
+   <p><b>Buff Squirrel:</b> This squirrel is actually a personal fitness trainer in its spare time.</p>
+   <p><b>Invisible Squirrel:</b> I bet you didn't see this coming!</p>
 </div>
 <br>
 <div class="flex">
