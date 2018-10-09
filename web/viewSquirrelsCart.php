959aa914c5e194e0c43314317172427a7c3c5137
akahmann <akahmann93@gmail.com>
2018-10-08 21:19:04 -0600

    Design

--- web/viewSquirrelsCart.php
+++ web/viewSquirrelsCart.php
@@ -52,57 +52,156 @@
          }
          if ($key == '001') {
             if ($value > 0) {
-               echo "<p class='purchase'>Happy Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Happy Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='001'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='001'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '002') {
             if ($value > 0) {
-               echo "<p class='purchase'>Heroic Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Heroic Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='002'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='002'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '003') {
             if ($value > 0) {
-               echo "<p class='purchase'>Evil Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Evil Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='003'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='003'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '004') {
             if ($value > 0) {
-               echo "<p class='purchase'>Fat Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Fat Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='004'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='004'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '005') {
             if ($value > 0) {
-               echo "<p class='purchase'>Knight Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Knight Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='005'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='005'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '006') {
             if ($value > 0) {
-               echo "<p class='purchase'>Superman Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Superman Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='006'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='006'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '007') {
             if ($value > 0) {
-               echo "<p class='purchase'>Thor Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Thor Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='007'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='007'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '008') {
             if ($value > 0) {
-               echo "<p class='purchase'>Giant Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Giant Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='008'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='008'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '009') {
             if ($value > 0) {
-               echo "<p class='purchase'>Hyena Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Hyena Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='009'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='009'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '010') {
             if ($value > 0) {
-               echo "<p class='purchase'>Buff Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Buff Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='010'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='010'>
+                     </form></div>";
+            }
             }
          }
          if ($key == '011') {
             if ($value > 0) {
-               echo "<p class='purchase'>Invisible Squirrel(s): $value <br></p>";
+               echo "<div class='group'><p class='purchase'>Invisible Squirrel(s): $value <br></p>";
+               echo "<form action='addSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='addBtn' value= '+'>
+                        <input type='hidden' name='item' value='011'>
+                     </form>";
+               echo "<form action='deleteSquirrelsBtn.php' method='post'>
+                        <input type='submit' class='deleteBtn' value= '-'>
+                        <input type='hidden' name='item' value='011'>
+                     </form></div>";
+            }
             }
          }
       }
