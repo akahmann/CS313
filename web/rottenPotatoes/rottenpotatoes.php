c7f3e43b6846dfdc7ebd815081c43f990ee2df9f
akahmann <akahmann93@gmail.com>
2018-10-20 16:59:58 -0600

    Update

--- web/rottenPotatoes/rottenpotatoes.php
+++ web/rottenPotatoes/rottenpotatoes.php
@@ -22,6 +22,39 @@ catch (PDOException $ex)
   die();
 }
 
+function findPic($pic) {
+   if ($pic == "The Legend of Zelda: Ocarina of Time") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/oot.jpg'" .
+           "alt='The Legend of Zelda: Ocarina of Time'>'";
+   }
+   else if ($pic == "Super Mario World") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/smw.jpg'" .
+           "alt='Super Mario World'>'";
+   }
+   else if ($pic == "Paper Mario") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/pmario.jpg'" .
+           "alt='Paper Mario'>'";
+   }
+   else if ($pic == "Final Fantasy VII") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/ff7.jpg'" .
+           "alt='Final Fantasy VII'>'";
+   }
+   else if ($pic == "Undertale") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/undertale.jpg'" .
+           "alt='Undertale'>'";
+   }
+   else if ($pic == "StarCraft 2") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/sc2.jpg'" .
+           "alt='StarCraft 2'>'";
+   }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +64,7 @@ catch (PDOException $ex)
    <link rel="stylesheet" type="text/css" href="rottenpotatoes.css">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>Rotten Potatoes</title>
+
 </head>
 <body>
 
@@ -41,15 +75,14 @@ catch (PDOException $ex)
       alt="Ocarina of Time"><br>
       <?php
          // $thing = "select * FROM games";
-         $qry = "select gm.name AS game_name, gr.name AS genre_name, " .
-                  "d.name AS developer_name" .
+         $qry = "select gm.gmname, gr.grname, d.dname" .
                   "FROM genres gr" .
                   "JOIN games gm ON gr.id = gm.genreId" .
                   "JOIN developers d ON gm.developerId = d.id";
          foreach ($db->query($qry) as $game)
          {
-            echo "<b> " . $game['game_name'] . "</b> " .
-            $game['genre_name'] . ": " . $game['developer_name'] . "<br>";
+            echo "<b> " . $game['gmname'] . "</b> " .
+            $game['grname'] . ": " . $game['dname'] . "<br>";
          }
       ?>
    </div>
