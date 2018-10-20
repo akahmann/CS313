66322c519f6f3bd3acbd57b89d091bca4934ec8f
akahmann <akahmann93@gmail.com>
2018-10-20 15:27:24 -0600

    Update

--- web/rottenPotatoes/rottenpotatoes.php
+++ web/rottenPotatoes/rottenpotatoes.php
@@ -11,7 +11,7 @@
    <h1>Rotten Potatoes</h1>
 
    <div class="midbody">
-      <img class="selectGamePic" src="rottenPotatoes/games/oot.jpg"
+      <img class="selectGamePic" src="/rottenPotatoes/games/oot.jpg"
       alt="Ocarina of Time"><br>
    </div>
 
