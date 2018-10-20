77047669c8c7b58b1fa69fddd49357f55fd3d577
akahmann <akahmann93@gmail.com>
2018-10-20 14:27:21 -0600

    Added database stuff and more

---
+++ web/rottenPotatoes/rottenpotatoes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="rottenpotatoes.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Rotten Potatoes</title>
+</head>
+<body>
+   <div class='midbody'>
+   <h1>Rotten Potatoes</h1>
+   </div>
+</body>
+</html>
