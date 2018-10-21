924eefc1816a37f18758427373888d70cd249f6d
akahmann <akahmann93@gmail.com>
2018-10-20 21:40:41 -0600

    Update

---
+++ web/rottenPotatoes/gamereview.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="rottenpotatoes.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Rotten Potatoes - Game Review</title>
+</head>
+<body>
+
+<div class="midbody">
+   BLAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+</div>
+
+</body>
+</html>
