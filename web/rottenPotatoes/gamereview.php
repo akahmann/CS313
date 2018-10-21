092d1bc3f54002474b2a8f75f2a462d16d75535f
akahmann <akahmann93@gmail.com>
2018-10-20 22:20:56 -0600

    Update

--- web/rottenPotatoes/gamereview.php
+++ web/rottenPotatoes/gamereview.php
@@ -71,10 +71,11 @@ function findPic($pic) {
 
 $name = $_GET['name'];
 findPic($name);
-
-$qry = "select text, g.name FROM reviews JOIN games g ON gameId = g.id WHERE g.name='$name'";
+echo "<br>";
+$qry = "select text, score, g.name FROM reviews JOIN games g ON gameId = g.id WHERE g.name='$name'";
       foreach ($db->query($qry) as $review) {
          echo "<p>" . $review['text'] . "</p>";
+         echo "Score Given: " . $review['score'] . "<br>";
       }
 
 ?>
