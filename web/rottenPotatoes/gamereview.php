f2402229fac80c35f8e1204e8538bef6a9c3a715
akahmann <akahmann93@gmail.com>
2018-10-20 21:43:25 -0600

    Update

--- web/rottenPotatoes/gamereview.php
+++ web/rottenPotatoes/gamereview.php
@@ -1,3 +1,38 @@
+<?php
+function findPic($pic) {
+   if ($pic == "The Legend of Zelda: Ocarina of Time") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/oot.jpg'" .
+           "alt='The Legend of Zelda: Ocarina of Time'>";
+   }
+   else if ($pic == "Super Mario World") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/smw.jpg'" .
+           "alt='Super Mario World'>";
+   }
+   else if ($pic == "Paper Mario") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/pmario.jpg'" .
+           "alt='Paper Mario'>";
+   }
+   else if ($pic == "Final Fantasy VII") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/ff7.jpg'" .
+           "alt='Final Fantasy VII'>";
+   }
+   else if ($pic == "Undertale") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/undertale.jpg'" .
+           "alt='Undertale'>";
+   }
+   else if ($pic == "StarCraft 2") {
+      echo "<img class='selectGamePic'" .
+           " src='/rottenPotatoes/games/sc2.jpg'" .
+           "alt='StarCraft 2'>";
+   }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,7 +44,12 @@
 <body>
 
 <div class="midbody">
-   BLAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+<?php
+
+$name = $_GET['name'];
+findPic($name);
+
+?>
 </div>
 
 </body>
