1ccc4e9896ad48363ef2197bcf2aad48499801d2
akahmann <akahmann93@gmail.com>
2018-10-25 16:12:44 -0600

    teamAct

---
+++ web/courses.php
@@ -0,0 +1,35 @@
+<?php
+
+   require('dbConnect.php');
+
+   $db = get_db();
+
+   $query = 'SELECT id, code, name FROM course';
+
+   $stmt = $db->prepare($query);
+   $stmt->execute();
+   $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Courses</title>
+</head>
+<body>
+   <h1>Course</h1>
+
+<?php
+
+   foreach ($course as $course) {
+      $id = $course['id'];
+      $name = $course['id'];
+      $code = $course['code'];
+
+      echo "<li><p>$code - $name</p></li>\n";
+   }
+
+?>
+
+</body>
+</html>
