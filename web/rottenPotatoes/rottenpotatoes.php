c4480e99033827efd131891f9bd46d95918c0a12
akahmann <akahmann93@gmail.com>
2018-10-27 14:50:59 -0600

    week6

--- web/rottenPotatoes/rottenpotatoes.php
+++ web/rottenPotatoes/rottenpotatoes.php
@@ -73,6 +73,7 @@ function findPic($pic) {
          }
          $average = $average / $count; //find the average score
          //findPic($name);
+         echo "This is $picLink <br>";
          echo "<img class='selectGamePic' src='$picLink' alt='$name'><br>";
          echo "<a href='https://cryptic-taiga-82259.herokuapp.com/rottenPotatoes/gamereview.php?id="
                . $game['id'] . "&name=" . $name . "'>" . $name . "</a>";
