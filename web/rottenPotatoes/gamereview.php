dfbf8db75636b78df1c39b35df4726c969cccf07
akahmann <akahmann93@gmail.com>
2018-11-05 19:19:19 -0700

    finishPotatoes

--- web/rottenPotatoes/gamereview.php
+++ web/rottenPotatoes/gamereview.php
@@ -7,6 +7,7 @@ $db = get_db();
 
 $id = htmlspecialchars($_GET['id']);
 $name = htmlspecialchars($_GET['name']);
+$picLink = htmlspecialchars($_GET['picLink']);
 
 $query = 'SELECT text, score, g.name FROM reviews JOIN games g ON gameId = g.id WHERE g.name=:name';
 
@@ -15,38 +16,38 @@ $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->execute();
 $reviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-function findPic($pic) {
-   if ($pic == "The Legend of Zelda: Ocarina of Time") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/oot.jpg'" .
-           "alt='The Legend of Zelda: Ocarina of Time'>";
-   }
-   else if ($pic == "Super Mario World") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/smw.jpg'" .
-           "alt='Super Mario World'>";
-   }
-   else if ($pic == "Paper Mario") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/pmario.jpg'" .
-           "alt='Paper Mario'>";
-   }
-   else if ($pic == "Final Fantasy VII") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/ff7.jpg'" .
-           "alt='Final Fantasy VII'>";
-   }
-   else if ($pic == "Undertale") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/undertale.jpg'" .
-           "alt='Undertale'>";
-   }
-   else if ($pic == "StarCraft 2") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/sc2.jpg'" .
-           "alt='StarCraft 2'>";
-   }
-}
+// function findPic($pic) {
+//    if ($pic == "The Legend of Zelda: Ocarina of Time") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/oot.jpg'" .
+//            "alt='The Legend of Zelda: Ocarina of Time'>";
+//    }
+//    else if ($pic == "Super Mario World") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/smw.jpg'" .
+//            "alt='Super Mario World'>";
+//    }
+//    else if ($pic == "Paper Mario") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/pmario.jpg'" .
+//            "alt='Paper Mario'>";
+//    }
+//    else if ($pic == "Final Fantasy VII") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/ff7.jpg'" .
+//            "alt='Final Fantasy VII'>";
+//    }
+//    else if ($pic == "Undertale") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/undertale.jpg'" .
+//            "alt='Undertale'>";
+//    }
+//    else if ($pic == "StarCraft 2") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/sc2.jpg'" .
+//            "alt='StarCraft 2'>";
+//    }
+// }
 ?>
 
 <!DOCTYPE html>
@@ -75,7 +76,8 @@ if (isset($_SESSION['username'])) {
 <div class="midbody">
 <?php
 
-findPic($name);
+//findPic($name);
+echo "<img class='selectGamePic' src='$picLink' alt='$name'>";
 echo "<br>";
 // $qry = "SELECT text, score, g.name FROM reviews JOIN games g ON gameId = g.id WHERE g.name=:name";
 // $db->prepare($qry);
--- web/rottenPotatoes/rottenpotatoes.php
+++ web/rottenPotatoes/rottenpotatoes.php
@@ -14,38 +14,38 @@ $stmt = $db->prepare($query);
 $stmt->execute();
 $games = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-function findPic($pic) {
-   if ($pic == "The Legend of Zelda: Ocarina of Time") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/oot.jpg'" .
-           "alt='The Legend of Zelda: Ocarina of Time'>";
-   }
-   else if ($pic == "Super Mario World") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/smw.jpg'" .
-           "alt='Super Mario World'>";
-   }
-   else if ($pic == "Paper Mario") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/pmario.jpg'" .
-           "alt='Paper Mario'>";
-   }
-   else if ($pic == "Final Fantasy VII") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/ff7.jpg'" .
-           "alt='Final Fantasy VII'>";
-   }
-   else if ($pic == "Undertale") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/undertale.jpg'" .
-           "alt='Undertale'>";
-   }
-   else if ($pic == "StarCraft 2") {
-      echo "<img class='selectGamePic'" .
-           " src='/rottenPotatoes/games/sc2.jpg'" .
-           "alt='StarCraft 2'>";
-   }
-}
+// function findPic($pic) {
+//    if ($pic == "The Legend of Zelda: Ocarina of Time") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/oot.jpg'" .
+//            "alt='The Legend of Zelda: Ocarina of Time'>";
+//    }
+//    else if ($pic == "Super Mario World") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/smw.jpg'" .
+//            "alt='Super Mario World'>";
+//    }
+//    else if ($pic == "Paper Mario") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/pmario.jpg'" .
+//            "alt='Paper Mario'>";
+//    }
+//    else if ($pic == "Final Fantasy VII") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/ff7.jpg'" .
+//            "alt='Final Fantasy VII'>";
+//    }
+//    else if ($pic == "Undertale") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/undertale.jpg'" .
+//            "alt='Undertale'>";
+//    }
+//    else if ($pic == "StarCraft 2") {
+//       echo "<img class='selectGamePic'" .
+//            " src='/rottenPotatoes/games/sc2.jpg'" .
+//            "alt='StarCraft 2'>";
+//    }
+// }
 
 ?>
 
@@ -99,7 +99,7 @@ function findPic($pic) {
       foreach ($games as $game) {
          $name = $game['name'];
          $picLink = $game['piclink'];
-         var_dump($game);
+         //var_dump($game);
          $qry2 = "SELECT score, g.name FROM reviews JOIN games g ON gameId = g.id WHERE g.name = '$name'";
          $average = 0;
          $count = 0;
@@ -109,11 +109,11 @@ function findPic($pic) {
          }
          $average = $average / $count; //find the average score
          $average = round($average, 2);
-         findPic($name);
-         echo "This is $picLink <br>";
+         //findPic($name);
+         //echo "This is $picLink <br>";
          echo "<img class='selectGamePic' src='$picLink' alt='$name'>";
          echo "<br><a href='https://cryptic-taiga-82259.herokuapp.com/rottenPotatoes/gamereview.php?id="
-               . $game['id'] . "&name=" . $name . "'>" . $name . "</a>";
+               . $game['id'] . "&name=" . $name . "&picLink=" . $picLink . "'>" . $name . "</a>";
          echo "<br>Average Score: $average";
          echo "<br><br><br>";
       }
